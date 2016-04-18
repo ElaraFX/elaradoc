b34e4bc7e1de8e4257fff5bc7ae6b65b6c714b54
Elvic <43492276@qq.com>
2016-04-18 21:26:40 +0800

    Add camera node.

---
+++ CPP_tutorials/tutorial_3_dof_camera/module.cpp
@@ -0,0 +1,19 @@
+/////////////////////////////////////////////////////
+// Module initialization and cleanup.
+//
+// Copyright 2013-2016 Rendease Co., Ltd. All Rights Reserved.
+//
+
+#include <ei_shaderx.h>
+
+// Will be called when the module is loaded.
+//
+EI_SHADER_API void module_init()
+{
+}
+
+// Will be called when the module is unloaded.
+//
+EI_SHADER_API void module_exit()
+{
+}
